e55fbab7e8b2357138797bbdecfdfdade0144680
Ansh Modi <160162966+Anshmodi03@users.noreply.github.com>
2024-12-08 15:32:13 +0530

    Delete models directory

--- models/Like.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const likeSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  postId: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
-});
-
-module.exports = mongoose.model("Like", likeSchema);
